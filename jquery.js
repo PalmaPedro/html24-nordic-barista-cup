9222e8cb8f1163e6b134b7630d330e39fa041b02
PalmaPedro <pmcpalma@gmail.com>
2020-04-29 12:33:02 +0200

    implementing jquery

--- jquery.js
+++ jquery.js
@@ -1,9 +1,9 @@
 /*
 jquery scripts:
-1 - load different image every time the webpage is loaded
-2 - search and highlight keyword(s) when found
-3 - read more/less button
-4 - pagination
+1 - load different image every time the webpage is loaded 
+2 - search and highlight keyword(s) when found 
+3 - read more/less button 
+4 - pagination ok!
 
 
 */
